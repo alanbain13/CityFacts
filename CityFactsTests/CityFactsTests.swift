ab34ae0f8493bd411254e081351a5b92d2d2fbeb
Alan Bainbridge <alan.bainbridge@antareez.com>
2025-03-02 13:10:54 +1100

    Initial Commit

---
+++ CityFactsTests/CityFactsTests.swift
@@ -0,0 +1,17 @@
+//
+//  CityFactsTests.swift
+//  CityFactsTests
+//
+//  Created by Alan Bainbridge on 2/3/2025.
+//
+
+import Testing
+@testable import CityFacts
+
+struct CityFactsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
